296e85cc6ef964681d10d365d124e5b904c41d9a
Simon <ooosaka1214@outlook.com>
2023-07-07 19:20:27 +0800

    初始化App.vue根组件内容

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>App.vue根组件</h1>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      // 任务列表数据
+      todolist: [
+        { id: 1, task: '周一早晨9点开会', done: false },
+        { id: 2, task: '周一晚上8点聚餐', done: false },
+        { id: 3, task: '准备周三上午的演讲稿', done: true }
+      ]
+    }
   }
 }
-</style>
+</script>
+
+<style lang="less" scoped></style>
