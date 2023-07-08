42216fa83f853018cfbe29511ef9f5199b4e2c18
Simon <ooosaka1214@outlook.com>
2023-07-08 15:55:54 +0800

    基于Bootstrap渲染TodoInput组件结构

--- src/components/todo-input/TodoInput.vue
+++ src/components/todo-input/TodoInput.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>TodoInput 组件</div>
+  <form class="form-inline">
+    <div class="input-group mb-2 mr-sm-2">
+      <!-- 输入框前缀 -->
+      <div class="input-group-prepend">
+        <div class="input-group-text">任务</div>
+      </div>
+      <!-- 文本输入框 -->
+      <input type="text" class="form-control" placeholder="请填写任务信息" style="width: 356px" />
+    </div>
+
+    <!-- 添加按钮 -->
+    <button type="submit" class="btn btn-primary mb-2">添加新任务</button>
+  </form>
 </template>
 
 <script>
